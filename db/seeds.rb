dae44dac9743479edc348606db4e27edc6132d89
Maggie hum <hum.maggie@gmail.com>
2023-03-01 16:52:16 -0500

    changes url in seed.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -40,13 +40,13 @@ doll3.save!
 images = [
   'https://res.cloudinary.com/dd1e1q8xa/image/upload/v1677615112/development/fk5bu0dcok9wv4quu6tufhw1m3ym.webp',
   'https://res.cloudinary.com/dd1e1q8xa/image/upload/v1677617010/development/1qs3fpfro82ifg9dnbutdmqx4z7g.webp',
-  'https://res.cloudinary.com/dd1e1q8xa/image/upload/v1677617544/production/mzecmu665oighpqn8m70br6u4irc.jpg',
-  'https://res.cloudinary.com/dd1e1q8xa/image/upload/v1677624521/production/kzjp1lur6c56x3x81fvfpk6s8ij3.jpg',
-  'https://res.cloudinary.com/dd1e1q8xa/image/upload/v1677624393/production/8id6z47bhixr9zhtr4kga114n9vi.jpg',
-  'https://res.cloudinary.com/dd1e1q8xa/image/upload/v1677624007/production/ysx8esd0wyz977pejj22cxmwi2t8.jpg',
-  'https://res.cloudinary.com/dd1e1q8xa/image/upload/v1677699471/photo-1612506001235-f0d0892aa11b_k1coc3.jpg',
-  'https://res.cloudinary.com/dd1e1q8xa/image/upload/v1677699516/photo-1613626253486-e2d1d9fd9bc9_yma2cs.jpg',
-  'https://res.cloudinary.com/dd1e1q8xa/image/upload/v1677699562/photo-1551875671-d593541a4b84_psnu8a.jpg'
+  'https://res.cloudinary.com/dd1e1q8xa/image/upload/v1677706851/production/ec38kq2k77eo0h70jo16axthzmfo.jpg',
+  'https://res.cloudinary.com/dd1e1q8xa/image/upload/v1677700618/photo-1533662958083-61eb6094138c_knbfle.jpg',
+  'https://res.cloudinary.com/dd1e1q8xa/image/upload/v1677700585/photo-1519892338195-90abfe1f6984_qnjumi.jpg',
+  'https://res.cloudinary.com/dd1e1q8xa/image/upload/v1677700555/photo-1635364796711-7018d19e5e58_ynba0d.jpg',
+  'https://res.cloudinary.com/dd1e1q8xa/image/upload/v1677700384/photo-1583656391619-488983c384e6_rpj8lr.jpg',
+  'https://res.cloudinary.com/dd1e1q8xa/image/upload/v1677700302/photo-1512839112604-a05d02098d9e_ywb1xh.jpg',
+  'https://res.cloudinary.com/dd1e1q8xa/image/upload/v1677699653/photo-1467103789230-f91a5ff8048a_lvn86a.jpg'
 ]
 
 images2 = [
@@ -75,15 +75,15 @@ images.each do |image|
   p "Doll with id #{doll.id}"
 end
 
-images2.each do |image|
-  doll = Doll.new(
-    name: Faker::Name.female_first_name,
-    price: Faker::Commerce.price,
-    description: Faker::Movies::Hobbit.quote,
-    location: Faker::Movies::Hobbit.location,
-    user: owner_two
-  )
-  file = URI.open(image)
-  doll.image.attach(io: file, filename: "nes.png", content_type: "image/png")
-  doll.save!
-end
+# images2.each do |image|
+#   doll = Doll.new(
+#     name: Faker::Name.female_first_name,
+#     price: Faker::Commerce.price,
+#     description: Faker::Movies::Hobbit.quote,
+#     location: Faker::Movies::Hobbit.location,
+#     user: owner_two
+#   )
+#   file = URI.open(image)
+#   doll.image.attach(io: file, filename: "nes.png", content_type: "image/png")
+#   doll.save!
+# end
