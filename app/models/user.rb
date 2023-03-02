66f950afd59872fe677753b939a57376cff26043
YOU PAN <sunnypanyou@gmail.com>
2023-03-02 12:57:03 -0500

    merge the status change funtion on the my_bookings page

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
   has_many :dolls
   has_many :bookings
+  has_many :owner_bookings, through: :dolls, source: :bookings
 end
