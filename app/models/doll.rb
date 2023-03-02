2f022cc724148eb7df38f768c9a14ec24a58462d
Adham Al Koukou <adhamalkoukou@gmail.com>
2023-03-02 10:17:49 -0500

    fixed some dependency and controller

--- app/models/doll.rb
+++ app/models/doll.rb
@@ -1,6 +1,6 @@
 class Doll < ApplicationRecord
   belongs_to :user
-  has_many :bookings
+  has_many :bookings, dependent: :destroy
   has_one_attached :image
   validates :name, :description, :price, :location, :image, presence: true
 end
