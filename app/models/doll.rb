190cd940df968b0e64336b0c9e7bbe13a7e6ca0d
Adham Al Koukou <adhamalkoukou@gmail.com>
2023-03-02 11:25:46 -0500

    added search bar to index

--- app/models/doll.rb
+++ app/models/doll.rb
@@ -3,4 +3,11 @@ class Doll < ApplicationRecord
   has_many :bookings
   has_one_attached :image
   validates :name, :description, :price, :location, :image, presence: true
+
+  include PgSearch::Model
+  pg_search_scope :search_by_name_and_location,
+  against: [ :name, :location ],
+  using: {
+    tsearch: { prefix: true } # <-- now `superman batm` will return something!
+  }
 end
